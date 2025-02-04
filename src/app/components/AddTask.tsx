905771b0fb65d532690a18fde0f59eb3f345e0e5
Preeti <preeti09cse@gmail.com>
2025-02-04 09:26:54 +0530

    added uuid

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -30,7 +30,7 @@ const AddTask = () => {
         onClick={() => setModalOpen(true)}
         className='btn btn-primary w-full'
       >
-        Add new task <AiOutlinePlus className='ml-2' size={18} />
+        Add new task <AiOutlinePlus  size={18} />
       </button>
 
       <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
